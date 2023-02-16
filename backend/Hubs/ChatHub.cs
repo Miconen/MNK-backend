72be43d9304d4d4d80ef1f88972477457c8cb1e1
Miconen <micropizza.mc@gmail.com>
2023-02-16 11:54:03 +0200

    Add functionality to ChatHub

--- backend/Hubs/ChatHub.cs
+++ backend/Hubs/ChatHub.cs
@@ -5,20 +5,23 @@ namespace backend.Hubs;
 
 public class ChatHub : Hub
 {
-    public void Send(string user, string message, string roomName)
+    public async Task SendMessage(string user, string message)
+        => await Clients.All.SendAsync("ReceiveMessage", user, message);
+
+    public async Task SendMessageToCaller(string user, string message)
+        => await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+
+    public async Task SendMessageToGroup(string user, string message, string groupName)
+        => await Clients.Group(groupName).SendAsync("ReceiveMessage", user, message);    
+    public async void JoinGroup(string user, string roomName)
     {
-        Clients.Group(roomName).SendAsync("ReceiveMessage", user, message);
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", user, "joined to " + roomName).ConfigureAwait(true);
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
     }
     
-    public void JoinRoom(string roomName, string user)
+    public async void LeaveGroup(string user, string roomName)
     {
-        Clients.Group(roomName).SendAsync("ReceiveMessage", user, " joined to " + roomName).ConfigureAwait(true);
-        Groups.AddToGroupAsync(Context.ConnectionId, roomName);
-    }
-    
-    public void LeaveRoom(string roomName, string user)
-    {
-        Clients.Group(roomName).SendAsync("ReceiveMessage", user, " left " + roomName).ConfigureAwait(true);
-        Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        await Clients.Group(roomName).SendAsync("ReceiveMessage", user, " left " + roomName).ConfigureAwait(true);
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
     }
 }
