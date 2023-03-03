db60b62a45ca1f669dceebe82df1f0dc3bef0333
Miconen <micropizza.mc@gmail.com>
2023-03-03 15:17:18 +0200

    Add user model for database

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace backend.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Password { get; set; }
+    public string? Role{ get; set; }
+}
