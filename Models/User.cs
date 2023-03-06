46e716f160ff02da3253a5889aba287adaff509c
Miconen <micropizza.mc@gmail.com>
2023-03-06 15:12:49 +0200

    Update database users model

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace backend.Models;
 
+[Table("Users")]
 public class User
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(12)]
     public string? Name { get; set; }
+    [Required]
+    [StringLength(12)]
     public string? Password { get; set; }
+    [StringLength(8)]
+    [DefaultValue("user")]
     public string? Role{ get; set; }
 }
