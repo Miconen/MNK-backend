8a8c750a0aaf6ccd2bb871b6d2ac6a37b1892a49
Miconen <micropizza.mc@gmail.com>
2023-03-10 13:13:33 +0200

    Improved error handling and stability of login and signup system

--- Controllers/SignupController.cs
+++ Controllers/SignupController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
 
+using BCrypt.Net;
+
 [Route("api/[Controller]")]
 [Route("api/[Controller]/[action]")]
 [ApiController]
@@ -21,26 +23,51 @@ public class SignupController : ControllerBase
     [Route("")]
     public IActionResult Singup([FromForm] AuthRequest signup)
     {
-        // Check if post request has correct parameters
-        bool NO_NAME = string.IsNullOrWhiteSpace(signup.Name);
-        bool NO_PASSWORD = string.IsNullOrWhiteSpace(signup.Password);
-        if (NO_NAME || NO_PASSWORD) return Ok(new { status = "Invalid post parameters" });
+        var errors = new List<string>();
 
-        int query = this._context.Users
-            .Where(u => u.Name == signup.Name)
-            .Count();
-        bool loggedIn = false;
-        if (query == 0) {
-            User user = new User(signup.Name, signup.Password);
-            this._context.Users
-                .Add(user);
-            this._context.SaveChanges();
-            loggedIn = true;
+        // Check if post request has correct parameters
+        if (string.IsNullOrWhiteSpace(signup.Name))
+        {
+            errors.Add("Name field cannot be empty");
         }
-        if (!loggedIn) return Ok(new { status = "Username is already taken" });
+        else if (string.IsNullOrWhiteSpace(signup.Password))
+        {
+            errors.Add("Password field cannot be empty");
+        }
+
+        // Hash the password
+        string passwordHash = "";
+        try
+        {
+            passwordHash = BCrypt.EnhancedHashPassword(signup.Password);
+        }
+        catch (SaltParseException ex)
+        {
+            errors.Add($"Error hashing password: {ex.Message}");
+        }
+
+        // Check if name is already taken
+        bool usernameTaken = this._context.Users
+            .Any(u => u.Name == signup.Name);
+
+        if (usernameTaken)
+        {
+            errors.Add("Username has already been taken");
+        }
+
+        // Return errors if any
+        if (errors.Count > 0)
+        {
+            return Ok(new { status = "Error", errors = errors });
+        }
+
+        // Add user to database
+        User user = new User(signup.Name!, passwordHash);
+        this._context.Users.Add(user);
+        this._context.SaveChanges();
 
         // Generate JWT
-        IAuthContainerModel model = JWTService.GetJWTContainerModel(signup.Name, signup.Password);
+        IAuthContainerModel model = JWTService.GetJWTContainerModel(signup.Name!, "user");
         IAuthService authService = new JWTService(model.SecretKey);
         string token = authService.GenerateToken(model);
 
